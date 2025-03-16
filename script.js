f4fc7c84cc76b73a8faa00854e50a6625f063165
testeguerrerolucas <lucashenriqueguerrero@gmail.com>
2025-03-15 22:14:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navMobile = document.querySelector('.nav-mobile');
-
-  menuToggle.addEventListener('click', function() {
-    navMobile.classList.toggle('active');
-  });
-});
